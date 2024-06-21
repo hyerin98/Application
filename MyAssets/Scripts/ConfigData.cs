1af622df550af8f1223e3c42bcf248641f3a22e0
hyerinleee <hyerin.lee@im-fine.co.kr>
2024-06-21 18:33:27 +0900

    6.21회사 - tcp 서버

--- MyAssets/Scripts/ConfigData.cs
+++ MyAssets/Scripts/ConfigData.cs
@@ -1,9 +1,18 @@
-[System.Serializable]
-public class ConfigData
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConfigData : MonoBehaviour
 {
-    public string num1 ="";
-    public string num2 = "";
-    public string num3 = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
-
-
